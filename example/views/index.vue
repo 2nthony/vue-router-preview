38075613a16807cdfc5a8440792f717d958d80ee
evila <ijoec123@gmail.com>
2019-01-28 00:58:08 +0800

    Update example.

--- example/views/index.vue
+++ example/views/index.vue
@@ -5,5 +5,13 @@
     <router-link to="/foo">preview-foo</router-link>
     <router-link to="/foo/hero">preview-foo-hero</router-link>
     <router-link to="/foo/hero1" :delay="1000" preview>404</router-link>
+
+    <router-link to="/foo">preview-foo</router-link>
+    <router-link to="/foo/hero">preview-foo-hero</router-link>
+    <router-link to="/foo/hero1" preview>404</router-link>
+
+    <router-link to="/foo">preview-foo</router-link>
+    <router-link to="/foo/hero">preview-foo-hero</router-link>
+    <router-link to="/foo/hero1" preview>404</router-link>
   </div>
 </template>
