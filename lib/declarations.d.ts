d6d7bc5066e7c56f6a87aa98debe478d8bd1dc0a
evila <ijoec123@gmail.com>
2019-02-02 19:49:28 +0800

    migrate to ts lightly

---
+++ lib/declarations.d.ts
@@ -0,0 +1,11 @@
+interface PreviewOpts {
+  componentName?: string
+  style?: object
+  preview?: boolean
+  delay?: string | number
+  scale?: string | number
+  safetyOffset?: number
+  keep?: boolean
+}
+
+type Html = HTMLElement
